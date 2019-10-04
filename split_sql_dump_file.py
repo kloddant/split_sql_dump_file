fefd7938d4d97b167fd9e62a561b0c41c69f9d18
Tony <kloddant@users.noreply.github.com>
2019-10-04 15:04:48 -0500

    Removed the default sql dump path

--- split_sql_dump_file.py
+++ split_sql_dump_file.py
@@ -3,7 +3,7 @@ import sys, re, os
 if sys.version_info[0] < 3:
 	raise Exception("""Must be using Python 3.  Try running "C:\\Program Files (x86)\\Python37-32\\python.exe" split_sql_dump_file.py""")
 
-sqldump_path = input("Enter the path to the sql dump file: ") or os.path.expanduser("~") + "\\Desktop\\popularwoodworking.sql"
+sqldump_path = input("Enter the path to the sql dump file: ")
 
 if not os.path.exists(sqldump_path):
 	raise Exception("Invalid sql dump path.  {sqldump_path} does not exist.".format(sqldump_path=sqldump_path))
